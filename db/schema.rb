c4c70bfa9e351ee9489cf5855143fb87638c09bc
shaily <shaily@github.com>
2019-08-05 10:52:54 +0530

    Form and query design pattern

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 ActiveRecord::Schema.define(version: 2019_08_02_110403) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "individuals", force: :cascade do |t|
-    t.string "name"
-    t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'individuals', force: :cascade do |t|
+    t.string 'name'
+    t.string 'phone_number'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
 end
